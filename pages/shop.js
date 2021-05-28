9e5c1f726de6d5a9557c798977ab9b5f58a7b503
Melissa Nelson <melissa.e.nelson@target.com>
2021-05-28 03:07:18 -0500

    add feature flags to shop page

--- pages/shop.js
+++ pages/shop.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
-import styles from './about.module.scss'
+import React from "react";
+import Layout from "../components/Layout/Layout";
+import styles from "./about.module.scss";
+import { useFlags } from "@happykit/flags/client";
 
 export default function Shop() {
-    return (
-        <Layout>
-            <div></div>
-        </Layout>
-    )
+  const { flags } = useFlags();
+
+  return (
+    <Layout>
+      <div>
+        {flags?.comingSoon && <p>Coming soon!</p>}
+        {flags?.pageContent && <p>Hello World</p>}
+      </div>
+    </Layout>
+  );
 }
