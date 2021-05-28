2da33d489425a0fca6921c23e21bf6df4198c95e
Melissa Nelson <melissa.e.nelson@target.com>
2021-05-27 22:57:45 -0500

    add navigation

---
+++ components/Layout/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from './Layout.module.scss'
+import Link from 'next/link'
+import Head from 'next/head'
+import { Button } from '@material-ui/core'
+import NavDrawer from '../../components/Drawer/Drawer.js'
+
+
+export default function Layout({ children }) {
+    return <div className={styles.container}>
+        <Head>
+            <title>Holkesvig</title>
+            <link rel="icon" href="/favicon.png" />
+        </Head>
+        <nav className={styles.navbar}>
+            <a href='/' className={styles.center}>Happy Horti</a>
+            <div className={styles.navigation}>
+                <Button className={`${styles.navItem} ${styles.desktop}`}><Link href="/about">About</Link></Button>
+                <Button className={`${styles.navItem} ${styles.desktop}`}><Link href="/travel">Guides</Link></Button>
+                <Button className={`${styles.navItem} ${styles.desktop}`}><Link href="/registry">Blog</Link></Button>
+                <Button className={`${styles.navItem} ${styles.desktop}`}><Link href="/faq">Shop</Link></Button>
+                <Button className={`${styles.navItem} ${styles.desktop}`}><Link href="/faq">FAQ</Link></Button>
+                <div className={styles.mobile}>
+                    <NavDrawer/>
+                </div>
+            </div>
+        </nav>
+        <div className={styles.mainContent}>{children}</div>
+    </div>
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import React from 'react'
+import './../styles/globals.css'
+import Head from 'next/head'
+import { StylesProvider } from '@material-ui/core/styles'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <StylesProvider injectFirst>
+        <Component {...pageProps}>
+            <Head>
+                <title>Happy Horti</title>
+            </Head>
+        </Component>
+    </StylesProvider>
 }
 
 export default MyApp
