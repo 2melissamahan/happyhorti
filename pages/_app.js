81e45d97e99d118d0cf8bb7a67f51f3d668c7860
Melissa Nelson <melissa.e.nelson@target.com>
2021-05-28 02:35:07 -0500

    update _appjs

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './../styles/globals.css'
 import Head from 'next/head'
 import { StylesProvider } from '@material-ui/core/styles'
-import { configure } from "@happykit/flags/config";
+// import { configure } from "@happykit/flags/config";
 
-configure({
-    envKey: process.env.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY
-});
+// configure({
+//     envKey: process.env.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY
+// });
 
 
 function MyApp({ Component, pageProps }) {
