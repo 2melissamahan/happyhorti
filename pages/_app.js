f6882abc6b08572b3d85676260e81c1e8609fd21
Melissa Nelson <melissa.e.nelson@target.com>
2021-05-28 02:30:43 -0500

    remove package-lock

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import React from 'react'
 import './../styles/globals.css'
 import Head from 'next/head'
 import { StylesProvider } from '@material-ui/core/styles'
+import { configure } from "@happykit/flags/config";
+
+configure({
+    envKey: process.env.NEXT_PUBLIC_FLAGS_ENVIRONMENT_KEY
+});
+
 
 function MyApp({ Component, pageProps }) {
     return <StylesProvider injectFirst>
